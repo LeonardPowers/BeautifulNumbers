13a02798c0662f96b18433e33bc637916a01c9c2
arsshpak <arsshpak@gmail.com>
2022-03-13 18:18:45 +0300

    Добавьте файлы проекта.

---
+++ BeautifulNumbers/MulticombinationExt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Kaos.Combinatorics;
+
+namespace BeautifulNumbers
+{
+    class MulticombinationExt:Multicombination
+    {
+
+        private int[] unicCountArray()
+        {
+            return this
+                .GroupBy(o => o)
+                .ToDictionary(g => g.Key, g => g.ToArray().Length)
+                .Values
+                .ToArray();
+        }
+
+        private void initPermutationSize()
+        {
+            var unicArray = unicCountArray();
+            permutationSize = Combinatoric.Factorial(this.Picks);
+            foreach (var item in unicArray)
+            {
+                permutationSize /= Combinatoric.Factorial(item);
+            }
+        }
+
+        public MulticombinationExt(Multicombination source):base(source)
+        {
+            initPermutationSize();
+            sum = this.Sum();
+        }
+        private long permutationSize { get; set; }
+
+        private int sum { get; }
+
+        public long PermutationSize => permutationSize;
+
+        public int Sum => sum;
+    }
+}
---
+++ BeautifulNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Kaos.Combinatorics;
+
+namespace BeautifulNumbers
+{
+    class Program
+    {
+        public static long CalculateBeautifulCount(int choices, int pick)
+        {
+            var mc = new Multicombination(choices, pick);
+            var listExt = mc.GetRows().Select(x => new MulticombinationExt(x));
+            var sumEqualsDictionary = listExt.GroupBy(o => o.Sum)
+                .ToDictionary(g => g.Key, g => g.ToArray());
+
+            long totalSum = 0;
+            foreach (var space in sumEqualsDictionary.ToList())
+            {
+                foreach (var itemLeft in space.Value)
+                {
+                    foreach (var itemRight in space.Value)
+                    {
+                        totalSum += itemLeft.PermutationSize * itemRight.PermutationSize;
+                    }
+                }
+            }
+            return totalSum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateBeautifulCount(13,6));
+        }
+    }
+}
